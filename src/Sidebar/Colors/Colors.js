10aeb4491d1f07a530be8d3b323128ac43de5f63
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-04-12 22:16:28 +0500

    Creating a E-commerce Search and Filtering System
    Creating a E-commerce Search and Filtering System with React

---
+++ src/Sidebar/Colors/Colors.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Colors.css'
+
+export default function Colors() {
+  return (
+    <div>
+      Colors
+    </div>
+  )
+}
---
+++ src/Sidebar/Price/Price.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Price.css'
+
+export default function Price() {
+  return (
+    <div>
+      Price
+    </div>
+  )
+}

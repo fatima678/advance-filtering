eaae6d8c95cb8ef9bd2cae26aab643fd5a1b8701
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-04-13 21:31:35 +0500

    Design the navbar section with css

--- src/Navigation/Nav.js
+++ src/Navigation/Nav.js
@@ -1,8 +1,30 @@
 import React from 'react'
 import './Nav.css'
+import { CiHeart } from "react-icons/ci";
+// import {AiPOutlineShoppingCart, AiOutlineUserAdd} from 'react-icons/ai'
+import { FaShoppingCart } from "react-icons/fa";
+import { AiOutlineUserAdd } from "react-icons/ai";
 
 export default function Nav() {
   return (
-    <div>Nav</div>
+    <nav>
+      <div className='nav-container'>
+        <input type='text'  className='search-input' placeholder='Enter your search shoes'></input>
+      </div>
+      <div className='profile-container'>
+        <a href='#'>
+          <CiHeart  className='nav-icons'/>
+        </a>
+        <a href='#'>
+          <FaShoppingCart className='nav-icons'/>
+        </a>
+        <a href='#'>
+          <AiOutlineUserAdd className='nav-icons'/>
+        </a>
+      </div>
+
+
+
+    </nav>
   )
 }

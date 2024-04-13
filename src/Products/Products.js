ceeaccbfffd0074b3c4a7ea1faf3337041a6bee6
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-04-13 22:10:15 +0500

    create the product section

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,10 +1,158 @@
 import React from 'react'
 import './Products.css'
+import { FaStar } from "react-icons/fa";
+import { BsBagHeartFill } from "react-icons/bs";
 
 export default function Products() {
   return (
-    <div>
-      Products
-    </div>
+    <>
+    <section className='card-container'> 
+    <section className='card'>
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt='Shoe' className='card-img'></img>
+      <div className='card-details'>
+        <h3 className='card-title'> Shoe</h3>
+        <section className='card-reviews'>
+          <FaStar className='ratings-start' />
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/>
+          <span className='total-reviews'>5</span>
+        </section>
+        <section className='card-price'>
+          <div className='price'>
+            <del>$300</del> 200
+          </div>
+          <div className='bag'>
+          <BsBagHeartFill  className='bag-icon'/>
+          </div>
+
+        </section>
+      </div>
+    </section>
+    {/* ------------------------------------------------------------------------------------------------------ */}
+
+
+
+    <section className='card'>
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt='Shoe' className='card-img'></img>
+      <div className='card-details'>
+        <h3 className='card-title'> Shoe</h3>
+        <section className='card-reviews'>
+          <FaStar className='ratings-start' />
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/>
+          <span className='total-reviews'>5</span>
+        </section>
+        <section className='card-price'>
+          <div className='price'>
+            <del>$300</del> 200
+          </div>
+          <div className='bag'>
+          <BsBagHeartFill  className='bag-icon'/>
+          </div>
+
+        </section>
+      </div>
+    </section>
+    {/* ------------------------------------------------------------------------------------ */}
+    <section className='card'>
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt='Shoe' className='card-img'></img>
+      <div className='card-details'>
+        <h3 className='card-title'> Shoe</h3>
+        <section className='card-reviews'>
+          <FaStar className='ratings-start' />
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/>
+          <span className='total-reviews'>5</span>
+        </section>
+        <section className='card-price'>
+          <div className='price'>
+            <del>$300</del> 200
+          </div>
+          <div className='bag'>
+          <BsBagHeartFill  className='bag-icon'/>
+          </div>
+
+        </section>
+      </div>
+    </section>
+    {/* -------------------------------------------------------------------------------------------------- */}
+    <section className='card'>
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt='Shoe' className='card-img'></img>
+      <div className='card-details'>
+        <h3 className='card-title'> Shoe</h3>
+        <section className='card-reviews'>
+          <FaStar className='ratings-start' />
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/>
+          <span className='total-reviews'>5</span>
+        </section>
+        <section className='card-price'>
+          <div className='price'>
+            <del>$300</del> 200
+          </div>
+          <div className='bag'>
+          <BsBagHeartFill  className='bag-icon'/>
+          </div>
+
+        </section>
+      </div>
+    </section>
+    {/* --------------------------------------------------------------------------------------------------------- */}
+    <section className='card'>
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt='Shoe' className='card-img'></img>
+      <div className='card-details'>
+        <h3 className='card-title'> Shoe</h3>
+        <section className='card-reviews'>
+          <FaStar className='ratings-start' />
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/>
+          <span className='total-reviews'>5</span>
+        </section>
+        <section className='card-price'>
+          <div className='price'>
+            <del>$300</del> 200
+          </div>
+          <div className='bag'>
+          <BsBagHeartFill  className='bag-icon'/>
+          </div>
+
+        </section>
+      </div>
+    </section>
+    {/* --------------------------------------------------------------------------------------------- */}
+    <section className='card'>
+      <img src="https://m.media-amazon.com/images/I/6125yAfsJKL._AC_UX575_.jpg" alt='Shoe' className='card-img'></img>
+      <div className='card-details'>
+        <h3 className='card-title'> Shoe</h3>
+        <section className='card-reviews'>
+          <FaStar className='ratings-start' />
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/> 
+          <FaStar className='ratings-start'/>
+          <span className='total-reviews'>5</span>
+        </section>
+        <section className='card-price'>
+          <div className='price'>
+            <del>$300</del> 200
+          </div>
+          <div className='bag'>
+          <BsBagHeartFill  className='bag-icon'/>
+          </div>
+
+        </section>
+      </div>
+    </section>
+
+    </section>
+    
+    
+    
+    
+    </>
   )
 }

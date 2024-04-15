ec305bddf9dae48a9a037d551bcfc2786ec35e8c
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-04-15 23:11:01 +0500

    Implement input filter functionality

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
+import { useState } from "react";
 import Nav from "./Navigation/Nav";
 import Products from "./Products/Products";
 import Recommended from "./Recommended/Recommended";
 import Sidebar from "./Sidebar/Sidebar";
-
-// import Colors from "./Sidebar/Colors/Colors";
-// import Price from "./Sidebar/Price/Price";
-
-
-
+//import data
+import products from './db/data'
 
 function App() {
+  const [selectedCategory, setSelectedCategory] = useState(null)
+  //----------input Filter-------------
+  const [query, setQuery] = useState("")
+
+  const handleInputChange = event =>{
+    setQuery(event.target.value)
+  }
+
+  const filteredItems = products.filter(product=> product.title.
+    toLocaleLowerCase().indexOf(query.toLocaleLowerCase())!==-1);
+
+     //----------Radio Filter-------------
+
+
   return (
     <>
     <Sidebar/>
       <Nav/>
       <Recommended/>
-      <Products/>
-     
-  
-      {/* <Colors/>
-      <Price/> */}
-     
-      
-      
+      <Products/> 
       </>
     
    

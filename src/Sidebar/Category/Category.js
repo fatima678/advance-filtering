2c2251e600a42eaeab1a44f62cd280f114eb3e6f
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-04-19 23:54:53 +0500

    Update the code

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -2,15 +2,35 @@ import React from 'react'
 import './Category.css'
 import Input from '../../components/Input'
 
-export default function Category() {
+export default function Category(handleChange) {
   return (
     <div>
-      <h3 className='sidebar-title'>Category</h3>
+      <h2 className='sidebar-title'>Category</h2>
       <div>
-        <Input/>
-        <Input/>
-        <Input/>
-        <Input/>
+        <label className='sidebar-label-container'>
+          <input onChange={handleChange} type='radio' value="" name='test'></input>
+          <span className='checkmark'></span>All
+        </label>
+        <Input 
+        handleChange={handleChange}
+         value="sneakers"
+         title="Sneakers"
+         name="test"
+         />
+          <Input 
+        handleChange={handleChange}
+         value="sandals"
+         title="Sandals"
+         name="test"
+         />
+          <Input 
+        handleChange={handleChange}
+         value="heels"
+         title="Heels"
+         name="test"
+         />
+       
+      
 
       </div>
     </div>
